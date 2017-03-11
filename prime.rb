bccb82962ea242fafa84a024226f7bde6965dc35
Govind Menon <govindmenon1995@yahoo.co.in>
2017-03-11 12:18:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  i = 2
+  number = 0-number if number < 0
+  while i <= number/2 do
+    if number%i == 0
+      return false
+    end
+  i+=1
+  end
+  return false if number==1 or number == 0
+  return true
+end
